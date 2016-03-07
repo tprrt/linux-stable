d19d41c41fc7b36446a102186d2bda0437a76d66
Alex Deucher <alexdeucher@gmail.com>
2016-03-07 18:40:45 -0500

    drm/amdgpu: fix topaz/tonga gmc assignment in 4.4 stable
    When upstream commit 429c45deae6e57f1bb91bfb05b671063fb0cef60
    was applied to 4.4 as d60703ca942e8d044d61360bc9792fcab54b95d0
    it applied incorrectly to the tonga_ip_blocks array rather than
    the topaz_ip_blocks array.  Fix that up here.
    
    Bug:
    https://bugzilla.kernel.org/show_bug.cgi?id=113951
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>

--- drivers/gpu/drm/amd/amdgpu/vi.c
+++ drivers/gpu/drm/amd/amdgpu/vi.c
@@ -1081,10 +1081,10 @@ static const struct amdgpu_ip_block_version topaz_ip_blocks[] =
 	},
 	{
 		.type = AMD_IP_BLOCK_TYPE_GMC,
-		.major = 8,
-		.minor = 0,
+		.major = 7,
+		.minor = 4,
 		.rev = 0,
-		.funcs = &gmc_v8_0_ip_funcs,
+		.funcs = &gmc_v7_0_ip_funcs,
 	},
 	{
 		.type = AMD_IP_BLOCK_TYPE_IH,
